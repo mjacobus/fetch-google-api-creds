bbe004951db1a5b05ed1b9d20404654464fb566d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-07-19 12:30:09 -0300

    Run the app - add a form

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
+
 gem 'dotenv'
 gem 'google-apis-gmail_v1'
 gem 'googleauth'
+gem 'puma', '~> 6.0'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -2,37 +2,77 @@ require 'sinatra'
 require 'googleauth'
 require 'google/apis/gmail_v1'
 require 'dotenv/load'
+require 'puma'
 
 set :port, 8080
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+RELEVANT_SCOPES = {
+  'Read Gmail' => 'https://www.googleapis.com/auth/gmail.readonly',
+  'Send Email' => 'https://www.googleapis.com/auth/gmail.send',
+  'Manage Drafts' => 'https://www.googleapis.com/auth/gmail.compose',
+  'Modify Gmail' => 'https://www.googleapis.com/auth/gmail.modify',
+  'Full Access' => 'https://www.googleapis.com/auth/gmail'
+}
+
 CREDENTIALS_PATH = 'credentials.json'
+
+enable :sessions
+set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
 
 get '/' do
   'Gmail API App'
 end
 
-get '/authorize' do
+get '/google/authorize' do
+  erb :authorize
+end
+
+post '/google/authorize' do
+  selected_scopes = params[:scopes]&.map { |scope| RELEVANT_SCOPES[scope] } || []
+  session[:selected_scopes] = selected_scopes
   client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
   token_store = Google::Auth::Stores::FileTokenStore.new(file: 'tokens.yaml')
-  authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPES, token_store)
+  authorizer = Google::Auth::UserAuthorizer.new(client_id, selected_scopes, token_store)
   user_id = 'default'
   credentials = authorizer.get_credentials(user_id)
 
   if credentials.nil?
-    redirect authorizer.get_authorization_url(base_url: request.url)
+    redirect authorizer.get_authorization_url(base_url: request.url.gsub('/authorize', '/oauth2callback'))
   else
     'Credentials already authorized'
   end
 end
 
-get '/oauth2callback' do
+get '/google/oauth2callback' do
+  selected_scopes = session[:selected_scopes]
   client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
   token_store = Google::Auth::Stores::FileTokenStore.new(file: 'tokens.yaml')
-  authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPES, token_store)
+  authorizer = Google::Auth::UserAuthorizer.new(client_id, selected_scopes, token_store)
   user_id = 'default'
   credentials = authorizer.get_and_store_credentials_from_code(
-    user_id:, code: params[:code], base_url: request.url
+    user_id:, code: params[:code], base_url: request.url.gsub('/oauth2callback', '/authorize')
   )
   'Authorization complete'
 end
+
+__END__
+
+@@authorize
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Authorize Gmail Scopes</title>
+</head>
+<body>
+  <h1>Select Scopes to Authorize</h1>
+  <form action="/google/authorize" method="post">
+    <% RELEVANT_SCOPES.each do |name, scope| %>
+      <div>
+        <input type="checkbox" id="<%= name %>" name="scopes[]" value="<%= name %>">
+        <label for="<%= name %>"><%= name %></label>
+      </div>
+    <% end %>
+    <button type="submit">Authorize</button>
+  </form>
+</body>
+</html>
